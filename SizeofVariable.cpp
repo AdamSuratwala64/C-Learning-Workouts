c20d51294b417e93a688909f0445430911a81faa
AdamSuratwala64 <69984474+AdamSuratwala64@users.noreply.github.com>
2020-11-23 18:27:59 +0530

    Program to display Size of each Variable Datatype

---
+++ SizeofVariable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char charvar;
+	int intvar;
+	float floatvar;
+	double doublevar;
+	
+	cout<<"Sizeof character is : "<<sizeof(charvar)<<endl;	
+	cout<<"Sizeof integer is : "<<sizeof(intvar)<<endl;	
+	cout<<"Sizeof float is : "<<sizeof(floatvar)<<endl;	
+	cout<<"Sizeof double is : "<<sizeof(doublevar)<<endl;	
+	return 0;
+}
