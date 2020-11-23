f761187b2152c12db818e1529dc2a20ea8711c38
AdamSuratwala64 <69984474+AdamSuratwala64@users.noreply.github.com>
2020-11-23 18:38:37 +0530

    Program to Display Product of Two Numbers

---
+++ ProductofTwo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float a=0.0, b =0.0;
+	
+	cout<<"Enter the first number : ";
+	cin>>a;
+	cout<<endl;
+	cout<<"Enter the second number : ";
+	cin>>b;
+	cout<<endl;
+	
+	float pro = a*b;
+	cout<<"The product of the two numbers is : " <<pro<<endl;
+	return 0;
+}
